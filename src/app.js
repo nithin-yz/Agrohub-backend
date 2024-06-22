13ba225d3910c4af06ae7ceeb5835a4c6d33322d
max <tomshift22@gmail.com>
2024-06-22 10:29:22 +0530

    made some relevant changes in folder

--- src/app.js
+++ src/app.js
@@ -4,14 +4,17 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 const connectDB = require("./config/db")
 require('dotenv').config()
+const authRoutes= require("")
+const farmerRoutes=require("")
+const adminRoutes =require("")
 
 const app=express()
 app.use(cors());
 app.use(express.json())
 connectDB()
-
-// app.use("/agrohub/farmer")
-// app.use("/agrohub/admin")
+app.use("/agrohub/auth", authRoutes)
+app.use("/agrohub/farmer",farmerRoutes)
+app.use("/agrohub/admin", adminRoutes)
 
 const Port= process.env.Port||5000
 app.listen(Port,()=>{console.log("Agro server Started"+"running on"+""+Port)})
